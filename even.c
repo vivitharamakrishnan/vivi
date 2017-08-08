48d69d855c37917c61e8fafce7d11c5068fd8f54
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2017-08-08 16:56:40 +0530

    even.c

---
+++ even.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n%2==0)
+{
+printf("the no is even");
+}
+else
+{
+printf("the no is even")
+}
+getch();
+}
