37c867d091d0bce398fed08f7f5004dc5154e23d
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2017-08-08 16:33:49 +0530

    sumn.c

---
+++ sumn.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,i,count;
+clrscr();
+printf("enter the count");
+scanf("%d",&count);
+for(i=0;i<=n;i++)
+{
+printf("enter the no ");
+scanf("%d",&n);
+sum=sum+n;
+printf("sum of the num is %d",sum);
+}
+getch();
+}
