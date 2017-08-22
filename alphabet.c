072be6bedcc8a4a8197b889c15220e4f28090be6
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2017-08-22 15:04:51 +0530

    alphabet.c

---
+++ alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char n;
+printf("enter the character");
+scanf("%c",&n);
+if((c>='a'&&c>='z')||(c>='A'&&c>='Z'))
+{
+printf("the charecter is alphabet");
+}
+else
+{
+printf("the charecter is not alphabet");
+}
+getch();
+}
