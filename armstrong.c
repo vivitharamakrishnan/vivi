a8d972dd5605172582454bcc71150b686c9911d7
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2017-08-29 15:58:18 +0530

    armstrong.c

---
+++ armstrong.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int n,rem,result;
+clrscr();
+printf("enter the number");
+scanf("%d",&n);
+if(n!=0)
+{
+rem=n%10;
+result=result+rem*rem*rem;
+n1=10;
+printf("armstrong number");
+}
+else
+{
+printf("not an armstrong number");
+}
+getch();
+}
