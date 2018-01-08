e5c1479d327360cc48a892b2152fce51e35ef99c
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-08 16:16:38 +0530

    mul.c

---
+++ mul.c
@@ -0,0 +1,13 @@
+void main()
+{
+        longint sum=0,i,n;
+        clrscr();
+        printf("\n Please Give The Value of N:  ");
+        scanf("%ld",&n);
+        for(i=1;i<=n;i++)
+        {
+            printf("\n 5 * %ld = %ld",i,5*i);
+        }
+        
+        getch();
+}
