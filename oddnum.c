8581c3743fc712f6a63c60d8be604d1066419db1
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-17 16:05:32 +0530

    oddnum.c

---
+++ oddnum.c
@@ -0,0 +1,19 @@
+
+#include<stdio.h>
+int main()
+{  
+    int n; 
+    printf("\nodd numbers between 2 intervals");  
+   for(n = 1; n <= 6; n++) 
+   {  
+      
+        if(n%1 == 0) 
+        { 
+            
+           printf("%d ",n); 
+           printf("\n odd number between two interval is");
+        }  
+    }  
+   
+    return 0;  
+} 
