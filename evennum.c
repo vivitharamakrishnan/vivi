a82455a6183085fcbff2a41bbc9806125ae24613
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-17 15:49:30 +0530

    evennum.c

---
+++ evennum.c
@@ -0,0 +1,17 @@
+int main()
+{  
+    int count,n; 
+    printf("\nEven numbers between 2 intervals");  
+   for(count = 1; count <= 5; counter++) 
+   {  
+      
+        if(count%2 == 0) 
+        { 
+            
+           printf("%d ",&count); 
+           printf("\n even number between two interval is",%d);
+        }  
+    }  
+   
+    return 0;  
+} 
