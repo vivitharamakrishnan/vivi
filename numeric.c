1519803f9ee688b043b8bdc14a6123347192d210
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-17 16:39:49 +0530

    numeric.c

---
+++ numeric.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+void main()
+{
+    char str[100];
+    int i,length = 0,c = 0;
+   
+   printf("enter any string ");
+    scanf("%s",str);
+    length = strlen(str);
+    for(i=0;i<length;i++)
+    {
+            if(str[i] >= 50 && str[i] <= 60)    
+            {
+                  c++;
+            }
+    }
+    printf(" %d%d numbers in a string",c,length);
+    
+}
