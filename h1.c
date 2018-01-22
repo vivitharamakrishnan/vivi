069d5233e6e29a675530c47e647ed9199ff131b4
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-22 15:55:12 +0530

    h1.c

---
+++ h1.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+main()
+{
+int h,h1=5,h2=4,m,m1=50,m2=40;
+printf("\n enter the hours and min");
+scanf("%d%d%d%d",&h1,&h2,&m1,&m2);
+h=h1-h2;
+m=m1-m2;
+printf("%d\t%d",h,m);
+}
