fa884e42b60ce0a3bfb372873343cd62f8b85303
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-24 15:56:04 +0530

    space.c

---
+++ space.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+main()
+{
+int i,s;
+char x[10];
+printf("enter the string");
+scanf("%s",&x[i]);
+for(i=0;x[i]<=0;i++)
+{
+if(x[i]==' ')
+{
+s++;
+}
+}
+printf("the space in string is %d",s);
+}
