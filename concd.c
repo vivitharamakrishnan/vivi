555b96941adb520366078d94fcff561753d59899
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-01-31 16:35:05 +0530

    concad.c

---
+++ concd.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+char a[50],b[40];
+printf("enter the first string\n");
+scanf("%s\n",a);
+printf("enter the sec string\n");
+scanf("%s\n",b);
+printf("%s%s\n",a,b);
+return 0;
+}
