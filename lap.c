656a677db1e7896bcb946610ac010b044a27c9b4
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-02-05 16:48:57 +0530

    lap.c

---
+++ lap.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+int n,i;
+char a[10]="laptop3";
+printf("enter num\n");
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+	printf("%c",a[i]);                                                                                                                                                                                                  
+}
+return 0;
+}
