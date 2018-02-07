6950beebc6f746f48b0fc4ff32a7138c5e757749
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-02-07 16:28:29 +0530

    prime4.c

---
+++ prime4.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+int n=0.i=1;
+printf("enter number\n");
+scanf("%d",&n);
+while(i!=0)
+{
+n=n+1;
+}
+if(n==1)
+{
+printf("not prime\n");
+}
+else
+{
+printf("prime\n");
+}
+return 0;
+}
+
