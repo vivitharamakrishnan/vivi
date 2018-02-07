5fbb5a511ef995427225583090e738d8bade6311
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-02-07 16:45:46 +0530

    vowel1.c

---
+++ vowel1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+char c[50]="eat";
+int i=0;
+if(c[i]=='a'||c[i]=='e'||c[i]=='i'||c[i]=='o'||c[i]=='u'||c[i]=='A'||c[i]=='E'||c[i]=='I'||c[i]=='O'||c[i]=='U')
+{
+printf("yes\n");
+}
+else
+{
+printf("no\n");
+}
+
+return 0;
+}
