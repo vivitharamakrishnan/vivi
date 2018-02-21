39ae2022a2fffc26cea8cded84faf12168f04d79
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-02-21 15:57:31 +0530

    kn.c

---
+++ kn.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,m,n1,m1,n2,m2;
+printf("enter n and m\n");
+scanf("%d%d\n",&n,&m);
+n=n-m;
+printf("%d",n);
+printf("enter n1 and m1\n");
+scanf("%d%d\n",&n1,&m1);
+n1=n1-m1;
+printf("%d",n1);
+printf("enter n2 and m2\n");
+scanf("%d%d\n",&n2,&m2);
+n2=n2-m2;
+printf("%d",n2);
+return 0;
+}
