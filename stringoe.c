8dc2e3b4d91a662d3f75dfd69b8493667c79e4b6
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-02-21 16:52:20 +0530

    stringoe.c

---
+++ stringoe.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+char a[50];
+int i,len;
+printf("enter the string\n");
+scanf("%s",&a);
+l=strlen(a[i]);
+for(i=0;i<len;i=i+2)
+{
+printf("%c",a[i]);
+}
+for(i=1;i<len;i=i+2)
+{
+printf("%c",a[i]);
+}
+return 0;
+}
