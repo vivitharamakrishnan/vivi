fdc3861767c949a0a5b37c5c6c1bc7de933566e4
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-03-07 16:39:58 +0530

    ap1.c

---
+++ ap1.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a,d,n,A,B,C,i,sum=0;
+printf("enter values\n");
+scanf("%d%d%d",&a,&d,&n);
+a=A;
+d=B;
+n=C;
+for(i=0;i<1;i++)
+{
+sum=sum+(a+i*B);
+}
+printf("%d",sum);
+return 0;
+}
