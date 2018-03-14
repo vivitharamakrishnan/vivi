80c1dc4b5e510e115cb07b1012462d25e69d303b
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-03-14 15:42:35 +0530

    ko.c

---
+++ ko.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+int main()
+{
+int n[20],N,K,i;
+printf("enter num");
+scanf("%d",&N);
+printf("enter K");
+scanf("%d",&K);
+for(i=0;i<N;i++)
+{
+scanf("%d",&n[i]);
+}
+for(i=0;i<N;i++)
+{
+if(K==n[i])
+{
+if(K%2==1)
+{
+printf("%d",K);
+}}}
+else
+{
+if(c[i+1]%2==1)
+{
+printf("%d",c[i+1]);
+}
+}
+return 0;
+}
