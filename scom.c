526c41372ab0b791bcaf39b77c0af06ba104f44c
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-03-14 16:48:00 +0530

    scom.c

---
+++ scom.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char a[10],b[10];
+int i,j,d,e;
+printf("enter two strings\n");
+scanf("%s%s",a,b);
+d=strlen(a);
+e=strlen(b);
+       if(a[i]==b[i])
+       {
+         printf("yes\n");
+       }
+       else
+       {
+         printf("no");
+       }
+       return 0;
+       }
