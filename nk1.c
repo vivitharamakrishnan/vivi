53c75919e068373db180a21d2e7341ca7bbeb867
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-03-14 16:55:04 +0530

    nk1.c

---
+++ nk1.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n,k;
+printf("enter n\n");
+scanf("%d",&n);
+printf("enter k\n");
+scanf("%d",&k);
+if(n/k==k)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+return 0;
+}
