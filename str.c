94013dffa03f5ff63def59c3d3dc1e15df6e3952
vivitharamakrishnan <30549578+vivitharamakrishnan@users.noreply.github.com>
2018-03-23 16:09:26 +0530

    str.c

---
+++ str.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+char str[10],str1[10];
+printf("enter string\n");
+scanf("%s",str);
+printf("enter string\n");
+scanf("%s",str1);
+printf("%s%s",str,str1);
+return 0;
+}
